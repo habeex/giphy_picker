b08698cfaa3490f8f99bde6d2d03db6896c9d6a9
Olorunishola Habeeb Bayo <olorunisholahabeeb@gmail.com>
2020-05-19 15:13:55 +0100

    Giphy costume Appbar

--- lib/src/widgets/giphy_preview_page.dart
+++ lib/src/widgets/giphy_preview_page.dart
@@ -6,19 +6,42 @@ import 'package:giphy_picker/src/widgets/giphy_image.dart';
 class GiphyPreviewPage extends StatelessWidget {
   final GiphyGif gif;
   final Widget title;
+  final Widget leading;
+  final List<Widget> actions;
+  final Color backgroundColor;
+  final Brightness brightness;
+  final IconThemeData iconTheme;
+  final IconThemeData actionsIconTheme;
+  final TextTheme textTheme;
+  final bool centerTitle;
   final ValueChanged<GiphyGif> onSelected;
 
   const GiphyPreviewPage(
-      {@required this.gif, @required this.onSelected, this.title});
+      {@required this.gif, @required this.onSelected, this.title,
+        this.leading,
+        this.actions,
+        this.backgroundColor,
+        this.brightness,
+        this.iconTheme,
+        this.actionsIconTheme,
+        this.textTheme,
+        this.centerTitle,}
+      );
 
   @override
   Widget build(BuildContext context) {
     final media = MediaQuery.of(context);
 
     return Scaffold(
-        appBar: AppBar(title: title, actions: <Widget>[
-          IconButton(icon: Icon(Icons.check), onPressed: () => onSelected(gif))
-        ]),
+        appBar: AppBar(
+          title: title, actions: actions,
+          leading: leading,
+          iconTheme: iconTheme,
+          brightness: brightness,
+          backgroundColor: backgroundColor,
+          actionsIconTheme: actionsIconTheme,
+          textTheme: textTheme,
+          centerTitle: centerTitle,),
         body: SafeArea(
             child: Center(
                 child: GiphyImage.original(
--- lib/src/widgets/giphy_search_page.dart
+++ lib/src/widgets/giphy_search_page.dart
@@ -2,14 +2,43 @@ import 'package:flutter/material.dart';
 import 'package:giphy_picker/src/widgets/giphy_search_view.dart';
 
 class GiphySearchPage extends StatelessWidget {
-  final Widget title;
 
-  const GiphySearchPage({this.title});
+  final Widget title;
+  final Widget leading;
+  final List<Widget> actions;
+  final Color backgroundColor;
+  final Brightness brightness;
+  final IconThemeData iconTheme;
+  final IconThemeData actionsIconTheme;
+  final TextTheme textTheme;
+  final bool centerTitle;
+
+  const GiphySearchPage({
+    this.title,
+    this.leading,
+    this.actions,
+    this.backgroundColor,
+    this.brightness,
+    this.iconTheme,
+    this.actionsIconTheme,
+    this.textTheme,
+    this.centerTitle,
+  });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: title),
+        appBar: AppBar(
+          title: title,
+          leading: leading,
+          iconTheme: iconTheme,
+          brightness: brightness,
+            actions: actions,
+          backgroundColor: backgroundColor,
+          actionsIconTheme: actionsIconTheme,
+          textTheme: textTheme,
+          centerTitle: centerTitle,
+        ),
         body: SafeArea(child: GiphySearchView(), bottom: false));
   }
 }
